dd946cc7a5ca513c4995a28c236a99ed3553b9de
John Doe <john@sample.com>
2019-12-27 09:41:43 +0800

    initial springmvc

---
+++ src/main/java/cn/itcast/utils/StringToDateConverter.java
@@ -0,0 +1,26 @@
+package cn.itcast.utils;
+
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * xx模块
+ */
+public class StringToDateConverter implements Converter<String,Date> {
+    public Date convert(String source){
+        if (source ==null){
+            throw new RuntimeException("请您输入数据");
+        }
+        DateFormat df=new SimpleDateFormat("yyyy-mm-dd");
+        try {
+            return df.parse(source);
+        } catch (ParseException e) {
+           throw new RuntimeException("数据类型转换出错误");
+        }
+    }
+}
