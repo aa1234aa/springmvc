36de02124e32ef517a38533d5632908a7f98d180
John Doe <john@sample.com>
2019-12-30 16:24:53 +0800

    initial springmvc

--- src/main/java/cn/itcast/controller/UserController.java
+++ src/main/java/cn/itcast/controller/UserController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -104,20 +106,36 @@ public class UserController {
         return "success";
     }
     @RequestMapping("/fileupload1")
-    public String fileupload1(HttpServletRequest request, MultipartFile upload) throws Exception{
-        System.out.println("fileupload1的方法执行了");
-        //上传的位置
-        String path=request.getSession().getServletContext().getRealPath("/uploads/");
-        //判断路径是否存在
-        File file=new File(path);
-        if (!file.exists()){
-            file.mkdir();
+    public String fileupload1(HttpServletRequest request, MultipartFile upload) throws IOException {
+        //upload是表单中文件name属性值,必须保持一致
+        System.out.println("testFileUpload...");
+        String realPath = request.getSession().getServletContext().getRealPath("/uploads");
+        File file = new File(realPath);
+        if(!file.exists()){
+            file.mkdirs();//创建文件夹
         }
-                //获取上传文件
-                String filename=upload.getOriginalFilename();
-                String uuid=UUID.randomUUID().toString().replace("-","");
-                filename=uuid+"_"+filename;
-                upload.transferTo(new File(path,filename));
-                return "success";
+        String filename = upload.getOriginalFilename(); //获取文件名
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");//生成uuid避免文件名重复导致冲突覆盖
+        filename=uuid+"_"+filename;
+        upload.transferTo(new File(file,filename));
+        return "success";
+    }
+    @RequestMapping("/upLoad")
+    @ResponseBody
+    public String upLoad(MultipartFile file) throws IllegalStateException, IOException {
+        // TODO Auto-generated method stub
+        // getOriginalFilename()获取原始文件名(在客户端时的文件名)
+        //getFileName() 在服务器上的文件名.
+        String filename = file.getOriginalFilename();
+        File file2 = new File("D:/imgs/"+filename);
+        //这里你可以进行重命名也可以把路径入库
+        //这里我就不做其他业务操作了
+
+        //MultipartFile中的transferTo不能使用两次
+        //因为http post文件流只可以接收读取一次，传输完毕则关闭流。
+        //可以把流保存为文件1，然后对文件1进行复制，移动等操作
+        //transferTo转存文件到指定路径
+        file.transferTo(file2);
+        return "success";
     }
 }
