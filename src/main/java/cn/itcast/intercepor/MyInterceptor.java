c7f5d79ff08eda884ff01057265fed8678f97691
anliping <anliping@bitnei.cn>
2020-01-01 17:06:06 +0800

    initial springmvc

--- src/main/java/cn/itcast/intercepor/MyInterceptor.java
+++ src/main/java/cn/itcast/intercepor/MyInterceptor.java
@@ -14,5 +14,6 @@ public class MyInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("执行拦截器");
         return true;
+
     }
 }
